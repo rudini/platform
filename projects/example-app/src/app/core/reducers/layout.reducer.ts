872c906801d7e27d40eee864c46ae18466c48f40
Roger Rudin <roger.rudin@gmail.com>
2018-11-16 09:41:55 +0100

    Update layout.reducer.ts

--- projects/example-app/src/app/core/reducers/layout.reducer.ts
+++ projects/example-app/src/app/core/reducers/layout.reducer.ts
@@ -1,33 +1,26 @@
 import {
   LayoutActions
 } from '@example-app/core/actions';
+import { ReducerBuilder } from '@lib/ngx-tools';
 
 export interface State {
   showSidenav: boolean;
 }
 
-const initialState: State = {
-  showSidenav: false,
-};
-
-export function reducer(
-  state: State = initialState,
-  action: LayoutActions.LayoutActionsUnion
-): State {
-  switch (action.type) {
-    case LayoutActions.LayoutActionTypes.CloseSidenav:
-      return {
-        showSidenav: false,
-      };
-
-    case LayoutActions.LayoutActionTypes.OpenSidenav:
-      return {
-        showSidenav: true,
-      };
-
-    default:
-      return state;
+const reducer = new ReducerBuilder<State>()
+  .handle(CloseSidenav, (state, action) => 
+    ({ ...state, showSidenav: false })
+  )
+  .handle(OpenSidenav, (state, action) => 
+    ({ ...state, showSidenav: true })
+  )
+  .build({
+    showSidenav: false,
   }
+);
+
+export function reducer(state: State, action: Action): State {
+    return reducer(state, action);
 }
 
 export const getShowSidenav = (state: State) => state.showSidenav;
