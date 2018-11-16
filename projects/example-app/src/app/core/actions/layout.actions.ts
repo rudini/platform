e9c9f33f4ec2498c54fcc76ed3ae0d8340b9e133
Roger Rudin <roger.rudin@gmail.com>
2018-11-16 09:36:44 +0100

    Update layout.actions.ts

--- projects/example-app/src/app/core/actions/layout.actions.ts
+++ projects/example-app/src/app/core/actions/layout.actions.ts
@@ -1,16 +1,9 @@
 import { Action } from '@ngrx/store';
 
-export enum LayoutActionTypes {
-  OpenSidenav = '[Layout] Open Sidenav',
-  CloseSidenav = '[Layout] Close Sidenav',
+export class OpenSidenav {
+  readonly type = '[Layout] Open Sidenav';
 }
 
-export class OpenSidenav implements Action {
-  readonly type = LayoutActionTypes.OpenSidenav;
+export class CloseSidenav {
+  readonly type = '[Layout] Close Sidenav';
 }
-
-export class CloseSidenav implements Action {
-  readonly type = LayoutActionTypes.CloseSidenav;
-}
-
-export type LayoutActionsUnion = OpenSidenav | CloseSidenav;
